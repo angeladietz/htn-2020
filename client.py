4bddb91b405ebf396dd7963c873640aa8abec7de
unknown <gohars1@mcmaster.ca>
2021-01-17 09:21:48 -0500

    Adding avatar functionality

--- client.py
+++ client.py
@@ -16,12 +16,17 @@ from pygame.locals import (
 from PodSixNet.Connection import ConnectionListener, connection
 from time import sleep
 import math
+import random
 
 class User(pygame.sprite.Sprite):
-  def __init__(self):
+  def __init__(self, avatar):
     super(User, self).__init__()
-    self.surf = pygame.Surface((50, 50))
-    self.surf.fill((255, 255, 255))
+    
+    self.avatar = 'inside\\' + avatar
+    print(self.avatar)
+    self.surf = pygame.image.load(self.avatar)
+    self.surf = pygame.transform.scale(self.surf,(100,100))
+    #self.surf.fill((255, 255, 255))
     self.rect = self.surf.get_rect()
     self.sound = None
 
@@ -38,6 +43,7 @@ class User(pygame.sprite.Sprite):
   
   def set_user_song(self, song):
     self.sound = song
+
 
     # Keep player on the screen
     # if self.rect.left < 0:
@@ -59,7 +65,7 @@ class Arena(ConnectionListener):
         # get a nickname from the user before starting
         print("Enter your nickname: ")
         self.my_name = stdin.readline().rstrip("\n")        
-        song = input("Enter your song: \n") 
+        song = input("Enter your song: \n")
 
 
         # launch our threaded input loop
@@ -67,24 +73,36 @@ class Arena(ConnectionListener):
 
         width, height = 800, 600
         self.screen = pygame.display.set_mode((width, height))
+
         pygame.display.set_caption("Arena")
         self.clock=pygame.time.Clock()
+
+
         self.users = {}
         self.user_channels = {}
         self.font = pygame.font.SysFont(None, 32)
-
-        self.me = User()
+        
+        self.avatars = ["cool-girl.png" , "cool-boy.png", "dog.png"]
+        avatar = self.avatars[random.randrange(0,2,1)]
+        self.me = User(avatar)
         self.users[self.my_name] = self.me
         self.me.set_user_song(song)
         while pygame.mixer.find_channel() is None:
           pygame.mixer.set_num_channels(pygame.mixer.get_num_channels()+1)
         self.user_channels[self.my_name] = pygame.mixer.find_channel()
         self.user_channels[self.my_name].play(pygame.mixer.Sound("sounds/" + self.me.sound))
-            
-        # pygame.mixer.music.play(-1)
-        connection.Send({"action": "nickname", "nickname": self.my_name, "x": self.me.rect.x, "y": self.me.rect.y, "sound": song})
-        print("Client started")
         
+     #   self.screen.fill(0,0,0)
+
+        # pygame.mixer.music.play(-1)
+        connection.Send({"action": "nickname", "nickname": self.my_name, "x": self.me.rect.x, "y": self.me.rect.y, "sound": song, "avatar": self.me.avatar })
+        print("Client started")
+    
+    # def getAvatar(self):
+    #     for avatar in self.avatars:
+    #       if (self.avatars[avatar] == False):
+    #         myavatar = 
+
     def update(self): 
         connection.Pump()
         self.Pump()
@@ -94,7 +112,7 @@ class Arena(ConnectionListener):
         self.screen.fill((150,20,200))
         self.pressed_keys = pygame.key.get_pressed()
         self.me.update(self.pressed_keys)
-        self.Send({"action": "pos", "name": self.my_name, "x":self.me.rect.x, "y":self.me.rect.y, "sound": self.me.sound})
+        self.Send({"action": "pos", "name": self.my_name, "x":self.me.rect.x, "y":self.me.rect.y, "sound": self.me.sound, "avatar" : self.me.avatar})
         self.users[self.my_name] = self.me
 
         for u_name, u_val in self.users.items():
@@ -142,7 +160,7 @@ class Arena(ConnectionListener):
         print("*** users: " + ", ".join([u for u in data['users']]))
 
     def Network_newuser(self, data):
-        new_user = User()
+        new_user = User(data['avatar'])
         new_user.rect.x = data['x']
         new_user.rect.y = data['y']
         new_user.sound = data['sound']
@@ -186,6 +204,7 @@ if __name__ == '__main__':
     else:
         host, port = sys.argv[1].split(":")
         arena = Arena(host, int(port))
+      #  arena.screen.blit(arena.background,(0,0))
         #pygame.mixer.music.play(1)
         while 1:
             arena.update()
